c1f20e299100404f56096f5af16bc1dd1bd85e67
suryanshgupta133@gmail.com <suryanshgupta133@gmail.com>
2023-07-06 00:53:41 +0530

    Initial commit

---
+++ Notes.js
@@ -0,0 +1,12 @@
+// 1) npm help in package install in react it also create the new app for react
+//  via npm create-react-app app_name
+
+// 2) It is recomended to use npx as it borrows the packages and we can use only till the time we need
+// use -> npx create-react-app app_name
+
+// 3) React is a Library
+
+// 4) Add bootstrap via npm i bootsrap
+// add import 'bootstrap/dist/js/bootstrap.js';
+// import 'bootstrap/dist/css/bootstrap.css';
+// in App.js
